8bb287986c05e6313bb9b9663dc6fbb359c577dc
annaperiscope <anna@periscopeworks.io>
2025-05-21 11:09:16 -0600

    local postgres setup

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,10 @@ import userRoutes from "./user/routes.js";
 import quoteRoutes from "./quote/routes.js";
 import orderRoutes from "./order/routes.js";
 import { ErrorHandler } from "./_global/errorHandler.js";
+import { PrismaClient } from "./generated/prisma";
 
 dotenv.config();
-
+const prisma = new PrismaClient();
 const app = express();
 const port: number = parseInt(process.env.PORT || "8080", 10);
 
@@ -20,6 +21,14 @@ const startServer = async () => {
         useNewUrlParser: true,
       } as mongoose.ConnectOptions,
     );
+
+    const bob = await prisma.test.upsert({
+      where: { name: "Bob" }, // look for Bob
+      update: {}, // do nothing if found
+      create: { name: "Bob" }, // create Bob if not found
+    });
+
+    console.log("✅ Inserted or found:", bob);
 
     app.use(express.json());
     app.use("/portal", portalRoutes);
